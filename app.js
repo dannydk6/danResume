54f47f7f9cc1d2fc8b68876f0659b80cf78e4f5d
root <root@vultr.guest>
2019-04-15 06:49:57 +0000

    Set up https

--- app.js
+++ app.js
@@ -32,6 +32,16 @@ app.use(function(req, res, next) {
   next(err);
 });
 
+app.use (function (req, res, next) {
+        if (req.secure) {
+                // request was via https, so do no special handling
+                next();
+        } else {
+                // request was via http, so redirect to https
+                res.redirect('https://' + req.headers.host + req.url);
+        }
+});
+
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
